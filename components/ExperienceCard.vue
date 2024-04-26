ae7fa2f23d2f874b317d697e0697fbc8d532f35c
roomdada <dsieroger@gmail.com>
2024-04-26 17:40:28 +0100

    refonte de la section mes esperiences

--- components/ExperienceCard.vue
+++ components/ExperienceCard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="md:w-1/2 bg-white px-6 py-4 shadow-lg rounded-lg border-1 mx-2 hover:border-2 hover:border-green-500">
-        <h3 class="text-xl font-bold mb-2">{{ props.post }} chez  {{ props.company }}</h3>
-        <p class="text-black  mb-2 text-justify">
-          {{ props.description }}
-        </p>
-        <div class="flex justify-between mt-4">
-          <span class="text-gray-600 font-semibold float-rigth">{{ props.year }}</span>
-        </div>
+<div class="w-full md:w-100 bg-white px-2 py-4 shadow-lg rounded-lg border-1 hover:border-2 hover:border-green-500">
+    <h3 class="text-md lg:text-xl font-bold mb-2">{{ props.post }} chez  {{ props.company }}</h3>
+    <p class="text-black mb-2 text-justify">
+        {{ props.description }}
+    </p>
+    <div class="flex justify-between mt-4">
+        <span class="text-gray-600 font-semibold float-rigth">{{ props.year }}</span>
     </div>
+</div>
 </template>
 <script setup>
   const props = defineProps({
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,8 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        {rel : 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"}
+        { rel : 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"},
+        { rel: "script", href:"https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"}
       ]
     }
   }
