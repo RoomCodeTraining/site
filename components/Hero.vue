ff8641a870be6b0872ccac1c76d8898791b0f80d
roomdada <dsieroger@gmail.com>
2024-05-08 17:09:52 +0100

    change linkedin link

--- components/Hero.vue
+++ components/Hero.vue
@@ -22,7 +22,7 @@
             </svg>
             Mon Github
         </a>
-        <a type="button" href="https://www.linkedin.com/in/da-sie-roger-889289192/" target="_blank" class="text-white bg-[#366ec7] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+        <a type="button" href="https://www.linkedin.com/in/dasieroger" target="_blank" class="text-white bg-[#366ec7] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 me-2" viewBox="0 0 20 20" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
                 <path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM8.339 18.337H5.667v-8.59h2.672v8.59zM7.003 8.574a1.548 1.548 0 1 1 0-3.096 1.548 1.548 0 0 1 0 3.096zm11.335 9.763h-2.669V14.16c0-.996-.018-2.277-1.388-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248h-2.667v-8.59h2.56v1.174h.037c.355-.675 1.227-1.387 2.524-1.387 2.704 0 3.203 1.778 3.203 4.092v4.71z"></path>
             </svg>
