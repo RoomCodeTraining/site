8421d1561f8227de88bebffbd49347f78760ce32
roomdada <dsieroger@gmail.com>
2024-07-10 17:35:40 +0100

    fix chatbot widget

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  plugins : [
+    { src : '~/plugins/brevo', mode : 'client' },
+  ],
   modules: [
       '@nuxtjs/tailwindcss',
       '@nuxt/image',
